f1d9bf735c1f471b9f0c032e102a00adadf28f34
Justin Saborouh <rubikmanjustin@gmail.com>
2022-09-07 10:27:17 -0700

    everything except patch

--- src/index.js
+++ src/index.js
@@ -12,4 +12,62 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  function fetchToys(url){
+    return fetch(url)
+    .then(res => res.json())
+  }
+
+  function createToys(url, body){
+    return fetch(url,{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: "application/json"
+      },
+      body: JSON.stringify(body),
+    })
+    .then(res => res.json())
+  }
+
+  function handleForm(e){
+    e.preventDefault();
+
+    const toy = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0
+    }
+
+    createToys('http://localhost:3000/toys', toy)
+    .then(renderToyCard)
+    .catch(e => console.error(e))
+  }
+
+  function renderToyCard(toyData){
+    const card = document.createElement('div')
+    const h2 = document.createElement('h2');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    const button = document.createElement('button');
+
+    h2.textContent = toyData.name;
+    img.src = toyData.image;
+    img.className = 'toy-avatar';
+    p.textContent = `${toyData.likes} likes`;
+    button.textContent = 'like';
+    card.className = 'card';
+  
+    card.append(h2,img,p,button);
+
+    button.addEventListener('click',()=>p.textContent = `${toyData.likes + 1} likes`);
+    document.querySelector('#toy-collection').append(card);
+  }
+
+  fetchToys('http://localhost:3000/toys')
+  .then(toys => toys.forEach(renderToyCard))
+  .catch(e => console.error(e))
+
+  document.querySelector('.add-toy-form').addEventListener('submit', handleForm)
+
 });
